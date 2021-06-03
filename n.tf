9fe61b2c06a024d2f508704e8fdd80b111d67245
Brent Woodruff <brent@fprimex.com>
2021-06-03 15:31:20 -0400

    this is actually what I wanted

--- n.tf
+++ n.tf
@@ -1,2 +1,12 @@
+terraform {
+  backend "remote" {
+    organization = "brentwoodruff"
+    workspaces {
+      name = "foo"
+    }
+  }
+}
+
 resource "null_resource" "n" {}
 resource "null_resource" "n2" {}
+resource "null_resource" "n3" {}
