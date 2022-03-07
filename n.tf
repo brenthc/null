4462beea575f677a5490baddc48409b6f2e4c1b0
Brent Woodruff <brent@hashicorp.com>
2022-03-07 14:23:20 -0500

    Update n.tf

--- n.tf
+++ n.tf
@@ -1,12 +1,30 @@
 terraform {
-  backend "remote" {
+  cloud {
     organization = "brentwoodruff"
     workspaces {
-      name = "foo"
+      name = "null"
+    }
+  }
+  required_providers {
+    null = {
+      source = "hashicorp/null"
+      version = "3.0.0"
     }
   }
 }
 
-resource "null_resource" "n" {}
-resource "null_resource" "n2" {}
-resource "null_resource" "n3" {}
+resource "null_resource" "n" {
+  triggers = {
+    uuid = uuid()
+  }
+  provisioner "local-exec" {
+    command = "ls -a"
+    #command = "cat *.tf.json"
+    #command = "python3 -c 'print(\"hello world\")'"
+    #command = "command -v vault"
+    #command = "cat /etc/resolv.conf"
+    #command = "host app.terraform.io"
+    #command = "free -m"
+    #command = "cat .terraform.lock.hcl"
+  }
+}
