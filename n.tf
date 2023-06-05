b035e9fee7cc82487f8c268f40e5f874d650601e
Brent Woodruff <brent@hashicorp.com>
2023-06-05 16:05:24 -0400

    updated

--- n.tf
+++ n.tf
@@ -5,12 +5,6 @@ terraform {
       name = "null"
     }
   }
-  required_providers {
-    null = {
-      source = "hashicorp/null"
-      version = "3.0.0"
-    }
-  }
 }
 
 resource "null_resource" "n" {
@@ -20,12 +14,5 @@ resource "null_resource" "n" {
 
   provisioner "local-exec" {
     command = "ls -a"
-    #command = "cat *.tf.json"
-    #command = "python3 -c 'print(\"hello world\")'"
-    #command = "command -v vault"
-    #command = "cat /etc/resolv.conf"
-    #command = "host app.terraform.io"
-    #command = "free -m"
-    #command = "cat .terraform.lock.hcl"
   }
 }
