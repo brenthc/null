06c9d4dec23e89cdf66f0641220cdff4ec9289f0
Brent Woodruff <brent@hashicorp.com>
2023-06-06 13:26:49 -0400

    generated bucket.tf

---
+++ bucket.tf
@@ -0,0 +1,12 @@
+# __generated__ by Terraform
+# Please review these resources and move them into your main configuration files.
+
+# __generated__ by Terraform from "upright-yak"
+resource "aws_s3_bucket" "b" {
+  bucket              = "upright-yak"
+  bucket_prefix       = null
+  force_destroy       = null
+  object_lock_enabled = false
+  tags                = {}
+  tags_all            = {}
+}
